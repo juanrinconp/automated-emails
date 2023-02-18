b16beb5a00f30963904258fafbb94fd6a13b9a0d
Juan Esteban Rincón Poveda <66634832+juanrinconp@users.noreply.github.com>
2023-02-18 16:32:12 -0500

    Add files via upload

---
+++ autofacturas.py
@@ -0,0 +1,19 @@
+import yagmail
+import pandas as pd
+import datetime
+import time
+
+while True:
+    if datetime.datetime.now().hour == 10 and datetime.datetime.now().minute == 27:
+        df = pd.read_excel('lista.xlsx')
+
+        for index, row in df.iterrows():
+            email = yagmail.SMTP(user="info@westpointsecurity.net", password="kmcvmyljrdumjypm")
+            email.send(to=row['email'],
+                       subject=f"Factura Conserjeria {row['mes']}",
+                       contents=f"Buenos dias estimados señores del {row['nombre']}, me permito enviarles la factura del servicio de conserjeria para el mes de {row['mes']}. Quedo pendiente a cualquier duda o inquietud. \n\n Atentemente,  \n \n Marcela Cobo \n Gerente Administrativa \n 3118945134",
+                       attachments=[f"{row['factura']}.txt" , "contacts.csv"])
+            print("Correos Enviados!")
+    time.sleep(60)
+
+
