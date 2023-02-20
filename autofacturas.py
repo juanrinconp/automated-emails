ee15fdd4a4580799ae3edce37ef4b8f833651ca7
Juan Esteban Rincón Poveda <66634832+juanrinconp@users.noreply.github.com>
2023-02-20 18:01:39 -0500

    Update autofacturas.py

--- autofacturas.py
+++ autofacturas.py
@@ -8,7 +8,7 @@ while True:
         df = pd.read_excel('lista.xlsx')
 
         for index, row in df.iterrows():
-            email = yagmail.SMTP(user="info@westpointsecurity.net", password="kmcvmyljrdumjypm")
+            email = yagmail.SMTP(user="info@xxxxxxxx.net", password="xxxxxxxxxxx")
             email.send(to=row['email'],
                        subject=f"Factura Conserjeria {row['mes']}",
                        contents=f"Buenos dias estimados señores del {row['nombre']}, me permito enviarles la factura del servicio de conserjeria para el mes de {row['mes']}. Quedo pendiente a cualquier duda o inquietud. \n\n Atentemente,  \n \n Marcela Cobo \n Gerente Administrativa \n 3118945134",
